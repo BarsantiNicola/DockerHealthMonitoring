689bed01a87fe5d71ef6939444447eea5ad76e77
Nicola <barsantinicola9@hotmail.it>
2021-06-11 00:21:40 +0200

    Created basic rabbitMQ class

--- Controller/components/DockerManager/rabbit.py
+++ Controller/components/DockerManager/rabbit.py
@@ -34,7 +34,7 @@ class client:
             self.channel.exchange_declare(exchange='health_system_exchange', exchange_type='direct')
             return True
 
-        except Exception:
+        except:
             self.connection = None
             self.channel = None
             return False
@@ -52,7 +52,7 @@ class client:
         return self.generate_channel()
 
     # send a unicast message to an antagonist. The hostname must be the hostname of the local selected machine
-    def send_antagonist_unicast(self, message, hostname) -> bool:
+    def send_antagonist_unicast(self, message, hostname, reply=True) -> bool:
 
         if self.address is None or message is None or hostname is None:
             return False
@@ -63,11 +63,14 @@ class client:
             self.channel.basic_publish(exchange='health_system_exchange', routing_key='antagonist.' + hostname,
                                        body=message)
             return True
-        except Exception:
+        except:
+            if reply is True:
+                self.generate_channel()
+                return self.send_antagonist_unicast(message, hostname, False)
             return False
 
     # send a broadcast message to all the antagonists
-    def send_antagonist_multicast(self, message):
+    def send_antagonist_multicast(self, message, reply=True):
 
         if self.address is None or message is None:
             return False
@@ -77,11 +80,14 @@ class client:
         try:
             self.channel.basic_publish(exchange='health_system_exchange', routing_key='antagonist', body=message)
             return True
-        except Exception:
+        except:
+            if reply is True:
+                self.generate_channel()
+                return self.send_antagonist_multicast(message, False)
             return False
 
     # send a unicast message to a manager. The hostname must be the hostname of the local selected machine
-    def send_manager_unicast(self, message, hostname) -> bool:
+    def send_manager_unicast(self, message, hostname, reply=True) -> bool:
 
         if self.address is None or message is None or hostname is None:
             return False
@@ -92,11 +98,14 @@ class client:
             self.channel.basic_publish(exchange='health_system_exchange', routing_key='manager.' + hostname,
                                        body=message)
             return True
-        except Exception:
+        except:
+            if reply is True:
+                self.generate_channel()
+                return self.send_manager_unicast(message, hostname, False)
             return False
 
     # send a broadcast message to all the docker managers
-    def send_manager_multicast(self, message) -> bool:
+    def send_manager_multicast(self, message, reply=True) -> bool:
 
         if self.address is None or message is None:
             return False
@@ -106,11 +115,14 @@ class client:
         try:
             self.channel.basic_publish(exchange='health_system_exchange', routing_key='manager', body=message)
             return True
-        except Exception:
+        except:
+            if reply is True:
+                self.generate_channel()
+                return self.send_manager_multicast(message, False)
             return False
 
     # send a message to the controller
-    def send_controller(self, message) -> bool:
+    def send_controller(self, message, reply=True) -> bool:
 
         if self.address is None or message is None:
             return False
@@ -120,25 +132,32 @@ class client:
         try:
             self.channel.basic_publish(exchange='health_system_exchange', routing_key='controller', body=message)
             return True
-        except Exception:
+        except:
+            if reply is True:
+                self.generate_channel()
+                return self.send_controller(message, False)
             return False
 
     # set a policy to receive message and set a thread to execute a callback function when new messages arrive
     # receiver_type: antagonist,manager,controller
     # callback_fun: function to be called whena message arrive
-    def allocate_receiver(self, receiver_type, callback_fun):
-        result = self.channel.queue_declare(queue='', exclusive=True)
-        queue_name = result.method.queue
+    def allocate_receiver(self, receiver_type, callback_fun) -> bool:
+        try:
+            result = self.channel.queue_declare(queue='', exclusive=True)
+            queue_name = result.method.queue
 
-        self.channel.queue_bind(exchange='health_system_exchange',
-                                queue=queue_name,
-                                routing_key=receiver_type)
+            self.channel.queue_bind(exchange='health_system_exchange',
+                                    queue=queue_name,
+                                    routing_key=receiver_type)
 
-        self.channel.queue_bind(exchange='health_system_exchange',
-                                queue=queue_name,
-                                routing_key=receiver_type+'.'+socket.gethostname())
-        self.channel.basic_consume(queue=queue_name, on_message_callback=callback_fun, auto_ack=True)
-        threading.Thread(target=self.channel.start_consuming).start()
+            self.channel.queue_bind(exchange='health_system_exchange',
+                                    queue=queue_name,
+                                    routing_key=receiver_type + '.' + socket.gethostname())
+            self.channel.basic_consume(queue=queue_name, on_message_callback=callback_fun, auto_ack=True)
+            threading.Thread(target=self.channel.start_consuming).start()
+            return True
+        except:
+            return False
 
     # example of valid callback function to be used into the allocate_receiver[TO BE DEFINED OUTSIDE RABBITCLIENT]
     @staticmethod
